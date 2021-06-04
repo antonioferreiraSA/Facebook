547822360d78ca21d156d360c23c06c25a9287ec
antonioferreiraSA <ferreiratoni112@gmail.com>
2021-06-04 17:19:54 +0200

    adding the backend to the post component

--- components/Posts.js
+++ components/Posts.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import Post from "./Post";
+import { db } from "../firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 function Posts() {
+ const [realtimePosts] = useCollection (
+   db.collection("posts").orderBy("timestamp", "desc")
+ );
   return (
     <div>
-      
+      {realtimePosts?.docs.map(post => (
+        <Post
+        key={post.id}
+        name={post.data().name}
+        message={post.data().message}
+        email={post.data().email}
+        timestamp={post.data().timestamp}
+        image={post.data().image}
+        postImage={post.data().postImage}
+        />
+      ))}
     </div>
   )
 }
 
 export default Posts
+
