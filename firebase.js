763a787f41b1d2d079949a3634d8b69086b1e69a
antonioferreiraSA <ferreiratoni112@gmail.com>
2021-06-04 11:30:41 +0200

    adding firebase config and storage

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import "firebase/storage";
+
+import firebase from 'firebse';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDApVnUhXhLFWphfX7JkoKNIcJ7UIlsoc4",
+  authDomain: "facebook-1d891.firebaseapp.com",
+  projectId: "facebook-1d891",
+  storageBucket: "facebook-1d891.appspot.com",
+  messagingSenderId: "862232510044",
+  appId: "1:862232510044:web:12d89ad9f4e7d1d4513099"
+};
